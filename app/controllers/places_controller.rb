db9b47f1a74c7849277ac6f82cb195b95553aa2e
Patrick Lewis <PatrickDanielLewis@gmail.com>
2017-04-19 18:46:52 +0000

    Added pagination, and form (not savable though) to app

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,22 @@
 class PlacesController < ApplicationController
     def index
+        
         @places = Place.all
+        @places = Place.paginate(:page => params[:page], :per_page => 10)
+        @places_last = Place.last
     end
+    
+    def new 
+        @place = Place.new
+    end    
+    
+    
 end
+
+    def self.search(place, current_page)
+        if term
+            page(current_page).where('name LIKE ?', "%#{term}%").order('id DESC')
+        else
+            page(current_page).order('id DESC') 
+        end
+    end
