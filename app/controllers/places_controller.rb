8714ad9fae70ca5746d6d8731a0fe16b78d9ed7a
Patrick Lewis <PatrickDanielLewis@gmail.com>
2017-04-20 21:26:29 +0000

    Added better styling to login screen

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-   before_action :authenicate_user!, only: [:new, :create]
+   before_action :authenticate_user!, only: [:new, :create] 
    
     def index
         
